5d0f71d6e3684bafc3e06242e0e73511721029c0
Kazi Anwar <kazisameen2702@gmail.com>
2023-11-28 16:50:21 -0500

    halfheapsort works but only if returning index 0

--- HalfHeapSort.cpp
+++ HalfHeapSort.cpp
@@ -1,46 +1,41 @@
 #include "HalfHeapSort.hpp"
 
-void percDown(std::vector<int>& heap, std::vector<int>::size_type hole)
+void percDown(std::vector<int>& heap, int hole, int size) 
 {
-    std::vector<int>::size_type child;
-    int temp = std::move(heap[hole]); // Save the value to be inserted into the hole
-    int end = heap.size();
+    int temp = heap[hole];
 
-    while (hole * 2 + 1 < end) 
+    while (hole * 2 + 1 < size) 
     {
-        child = hole * 2 + 1;
-
-        // Compare left and right child, choose the larger one
-        if (child + 1 < end && heap[child] < heap[child + 1]) 
+        int child = hole * 2 + 1; // Left child
+        // Selecting the larger child. If they are equal, left child is chosen.
+        if (child != size - 1 && heap[child] < heap[child + 1]) 
         {
-            ++child;
+            child++; // Right child if larger
         }
 
-        // Compare the larger child with the value to be inserted
         if (heap[child] > temp) 
         {
-            heap[hole] = std::move(heap[child]);
+            heap[hole] = heap[child];
+            hole = child;
         } 
+        
         else 
         {
             break;
         }
-
-        hole = child;
     }
-
-    heap[hole] = std::move(temp); // Insert the value into the correct place
+    heap[hole] = temp;
 }
 
 // parameter "hole" is the index of the hole.
 // percDown precondition: value to be inserted into hole is stored in heap at index 0. The hole itself may be in an unspecified state - i.e. it doesn't matter what's in it since you'll be overwriting it anyway.
 // percDown postcondition: hole has been moved into correct place and value has been inserted into hole.
-void buildHeap(std::vector<int>& heap)
+void buildHeap(std::vector<int>& heap) 
 {
-    int end = heap.size();
-    for (std::vector<int>::size_type i = end / 2; i >= 1; --i) 
+    int size = heap.size();
+    for (int i = (size - 2) / 2; i >= 0; --i) 
     {
-        percDown(heap, i - 1);
+        percDown(heap, i, size);
     }
 }
 
@@ -48,14 +43,18 @@ int halfHeapSort(std::vector<int>& nums, int& duration) //func delcr
 {
     auto start = std::chrono::high_resolution_clock::now(); //start time of algo
 
-    // Move the first element to the back
-    std::swap(nums[1], nums[nums.size() - 1]);
+    // Move the first element to the end
+    nums.push_back(nums[0]);
+    nums.erase(nums.begin());
+
     buildHeap(nums);
 
-    for (std::vector<int>::size_type i = 1; i < nums.size() / 2; ++i) 
-    {
-        std::swap(nums[1], nums[nums.size() - 1 - i]);
-        percDown(nums, 1); // Start with index 1
+    int size = nums.size();
+    // Removing elements until half of the original size
+    while (size > (nums.size() / 2)) {
+        std::swap(nums[0], nums[size - 1]);
+        size--;
+        percDown(nums, 0, size);
     }
 
     auto end = std::chrono::high_resolution_clock::now(); //yea we know
@@ -70,12 +69,13 @@ int halfHeapSort(std::vector<int>& nums, int& duration) //func delcr
     }
     std::cout << "\n";
 
+    return nums[0];
+
     //find & return median of the sorted vector
     //return nums[1]; //if u make this 0, u get the correct thing but like he wants it to start at 1
     // Return the median of the sorted vector
     // return (nums.size() % 2 == 0) ? (nums[nums.size() / 2] + nums[nums.size() / 2 + 1]) / 2 : nums[nums.size() / 2 + 1];
-    return nums[nums.size()/2];
-
+    //return nums[nums.size()/2];
     // int size = nums.size();
     // if (size % 2 == 0) //for even-sized vectors
     // {
@@ -87,6 +87,5 @@ int halfHeapSort(std::vector<int>& nums, int& duration) //func delcr
     //     //for odd-sized vectors, return middle element
     //     return nums[size / 2];
     // }
-
     //side note: we probably use the helper here because its recursive and we dont want to call the main function more than once because it prints statements and times clocks?
 }

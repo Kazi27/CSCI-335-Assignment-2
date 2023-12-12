6af52083aadcee5f47429265c5cd87debfd2c59a
Kazi Anwar <kazisameen2702@gmail.com>
2023-12-12 02:14:28 -0500

    trying worst case

--- WorstCaseQuickSelect.hpp
+++ WorstCaseQuickSelect.hpp
@@ -57,9 +57,10 @@ std::vector<int> worstCaseQuickSelect()
 {
     std::vector<int> worstCaseInput(20000);
 
-    for (int i = 0; i < 20000; ++i)
+    for (int i = 0; i < 10000; ++i)
     {
-        worstCaseInput[i] = 20000 - i;
+        worstCaseInput[i] = i + 1;
+        worstCaseInput[i + 10000] = 20000 - i;
     }
 
     return worstCaseInput;

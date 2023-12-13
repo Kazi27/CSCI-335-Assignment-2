737e7e6e0fb1a7b07a175a4633b99d7cc8062625
Kazi Anwar <kazisameen2702@gmail.com>
2023-12-13 00:49:06 -0500

    finalize halfheapsort

--- HalfHeapSort.hpp
+++ HalfHeapSort.hpp
@@ -84,7 +84,10 @@
 // }
 
 // #endif
-#include <iostream>
+
+//Name: Kazi Sameen Anwar
+//CSCI - 335 Assignment 2
+#include <iostream> //guards
 #include <vector>
 #include <algorithm>
 #include <chrono>
@@ -92,19 +95,26 @@
 // parameter "hole" is the index of the hole.
 // percDown precondition: value to be inserted into hole is stored in heap at index 0. The hole itself may be in an unspecified state - i.e. it doesn't matter what's in it since you'll be overwriting it anyway.
 // percDown postcondition: hole has been moved into correct place and value has been inserted into hole.
-void percDown(std::vector<int>& heap, std::vector<int>::size_type hole) {
+void percDown(std::vector<int>& heap, std::vector<int>::size_type hole) 
+{
     int leftChild;
     int temp = heap[0];
 
-    for(; hole * 2 < heap.size(); hole = leftChild) {
+    for(; hole * 2 < heap.size(); hole = leftChild) 
+    {
         leftChild = hole * 2;
-        if(leftChild + 1 < heap.size() && heap[leftChild] > heap[leftChild + 1]) {
+        if(leftChild + 1 < heap.size() && heap[leftChild] > heap[leftChild + 1]) 
+        {
             leftChild++;
         }   
-        if(temp > heap[leftChild]) {
+
+        if(temp > heap[leftChild]) 
+        {
             heap[hole] = heap[leftChild];
         }
-        else{
+
+        else
+        {
             break;
         }
     }
@@ -112,35 +122,35 @@ void percDown(std::vector<int>& heap, std::vector<int>::size_type hole) {
     heap[hole] = temp;
 }
 
-void buildHeap(std::vector<int>& heap) {
-    for(int i = (heap.size()-1)/2; i > 0; i--) {
+void buildHeap(std::vector<int>& heap) 
+{
+    for(int i = (heap.size()-1)/2; i > 0; --i) 
+    {
         heap[0] = heap[i];
         percDown(heap, i);
     }
 }
 
-int halfHeapSort(std::vector<int>& nums, int& duration) {
-    //get starting time
-    auto start = std::chrono::steady_clock::now();
+int halfHeapSort(std::vector<int>& nums, int& duration) // func delcr
+{
+    auto start = std::chrono::steady_clock::now(); // start_time time of algo
 
-    //add first element to the back
-    nums.push_back(nums[0]);
+    nums.push_back(nums[0]); //move first element to the end
 
-    //buildHeap
-    buildHeap(nums);
+    buildHeap(nums); //partial heapsort to get bottom n/2 elements in ascending order
 
     int middle = (nums.size()-2)/2;
 
-    for(int i = 0; i < middle; i++) {
+    for(int i = 0; i < middle; i++) 
+    {
         nums[0] = nums[nums.size() - 1];
         nums.pop_back();
         percDown(nums, 1);
     }
 
-    //get finishing time
     auto end = std::chrono::steady_clock::now();
 
     duration = std::chrono::duration<double, std::milli> (end - start).count();
 
-    return nums[1];
+    return nums[1]; //median at nums[1] as per project specs
 }
